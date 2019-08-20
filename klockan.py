8b316786b6c84b3b7d073e237c752acc378d8a30
Martin Hellspong <martin.hellspong@factor10.com>
2019-08-20 11:31:07 +0200

    Textual description of time

--- klockan.py
+++ klockan.py
@@ -131,6 +131,7 @@ pygame.key.set_repeat(500, 50)
 # available = pygame.font.get_fonts()
 # print(available)
 font = pygame.font.SysFont("menlottc", 36)
+font2 = pygame.font.SysFont("menlottc", 24)
 game_display = pygame.display.set_mode((display_width, display_height), 0, 32)
 game_display.fill(GRAY)
 pygame.display.set_caption('Klockan')
@@ -139,7 +140,7 @@ klockan = Klocka()
 digital = font.render("00:00", True, GREEN)
 
 def event_handler(klocka):
-    global display_delta, display_offset
+    global display_delta, display_offset, display_time
     mul = 1
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
@@ -162,6 +163,9 @@ def event_handler(klocka):
                 mul *= 4
         if event.type == KEYDOWN and event.key == K_m:
             klocka.floating_minutes = not klocka.floating_minutes
+        if event.type == KEYDOWN and event.key == K_n:
+            display_offset = 0
+            display_time = time.time()
         if event.type == KEYDOWN and event.key == K_r:
             if not klocka.running:
                 display_offset = display_time - time.time()
@@ -174,6 +178,42 @@ def event_handler(klocka):
         if event.type == KEYDOWN and (event.key == K_RIGHT or event.key == K_e or event.key == K_w or event.key == K_q):
             display_delta = 0
             display_offset += mul
+
+def draw_text():
+    pygame.draw.rect(game_display, GRAY, (0, 550, display_width, 200))
+    timme = klockan.now.tm_hour
+    minut = klockan.now.tm_min
+    if timme > 12: timme -= 12
+    namn = ["tolv", "ett", "två", "tre", "fyra", "fem", "sex", "sju", "åtta", "nio", "tio", "elva", "tolv", "ett"]
+    period = ["natten", "tidiga morgonen", "morgonen", "förmiddagen", "dagen", "eftermiddagen", "kvällen", "sena kvällen", "natten"]
+    minuter = ["", "fem över ", "tio över ", "kvart över ", "tjugo över ", "fem i halv ", "halv ", "fem över halv ", "tjugo i ", "kvart i ", "tio i ", "fem i ", ""]
+    precision = ["     ", "ung. ", "ung. ", "ung. ", "ung. "]
+    text1 = [
+        font2.render(precision[minut % 5], True, PINK),
+        font2.render(minuter[(minut+2)//5], True, PINK),
+        font2.render(namn[timme + (minut+35)//60], True, BLUE)
+    ]
+    text2 = [
+        font2.render("     på ", True, BLACK),
+        font2.render(period[klockan.now.tm_hour // 3], True, BLACK)
+    ]
+    t1w = 0
+    for dig in text1:
+        t1w += dig.get_width()
+
+    offs = 50
+    for dig in text1:
+        game_display.blit(dig, (offs, 550 - dig.get_height() // 2))
+        offs += dig.get_width()
+
+    t2w = 0
+    for dig in text2:
+        t2w += dig.get_width()
+
+    offs = 50
+    for dig in text2:
+        game_display.blit(dig, (offs, 580 - dig.get_height() // 2))
+        offs += dig.get_width()
 
 def draw_digital():
     pygame.draw.rect(game_display, GRAY, (0, 400, display_width, 200))
@@ -209,6 +249,7 @@ while True:
     if newSecond():
         draw_clock()
         draw_digital()
+        draw_text()
     # game_display.fill((100,0,0))
     #pygame.draw.rect(game_display, (100,0,100), Rect(10, 10, 20, 20))
     #pygame.draw.polygon(game_display, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
