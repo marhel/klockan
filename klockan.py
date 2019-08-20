85ce7ceadbe7aa12d1d514e9c6c737c0d4062d6e
Martin Hellspong <martin.hellspong@factor10.com>
2019-08-20 10:19:37 +0200

    Running or not

--- klockan.py
+++ klockan.py
@@ -13,6 +13,7 @@ TRANSPARENT = (0, 0, 0, 0)
 
 class Klocka(pygame.Surface):
     now = time.localtime()
+    running = True
     size = (400, 400)
     floating_minutes = False
 
@@ -141,11 +142,7 @@ def event_handler(klocka):
     global display_delta, display_offset
     mul = 1
     for event in pygame.event.get():
-        if event.type == QUIT or (
-             event.type == KEYDOWN and (
-              event.key == K_ESCAPE or
-              event.key == K_q
-             )):
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             pygame.quit()
             quit()
         if event.type == KEYDOWN and event.key == K_UP:
@@ -153,18 +150,28 @@ def event_handler(klocka):
         if event.type == KEYDOWN and event.key == K_DOWN:
             display_delta -= 1
         if event.type == pygame.KEYDOWN:
-            if event.mod == pygame.KMOD_NONE:
+            if event.key == K_d or event.key == K_e:
                 mul = 1
+            if event.key == K_s or event.key == K_w:
+                mul = 73
+            if event.key == K_a or event.key == K_q:
+                mul = 73 * 12
             if event.mod & pygame.KMOD_SHIFT:
-                mul *= 73
+                mul *= 2
             if event.mod & pygame.KMOD_CTRL:
-                mul *= 12
+                mul *= 4
         if event.type == KEYDOWN and event.key == K_m:
             klocka.floating_minutes = not klocka.floating_minutes
-        if event.type == KEYDOWN and event.key == K_LEFT:
+        if event.type == KEYDOWN and event.key == K_r:
+            if not klocka.running:
+                display_offset = display_time - time.time()
+            else:
+                display_offset = 0
+            klocka.running = not klocka.running
+        if event.type == KEYDOWN and (event.key == K_LEFT or event.key == K_d or event.key == K_s or event.key == K_a):
             display_delta = 0
             display_offset -= mul
-        if event.type == KEYDOWN and event.key == K_RIGHT:
+        if event.type == KEYDOWN and (event.key == K_RIGHT or event.key == K_e or event.key == K_w or event.key == K_q):
             display_delta = 0
             display_offset += mul
 
@@ -194,7 +201,11 @@ while True:
     event_handler(klockan)
     klockan.now = time.localtime(display_time)
     display_offset += display_delta
-    display_time = time.time() + display_offset
+    if klockan.running:
+        display_time = time.time() + display_offset
+    else:
+        display_time += display_offset
+        display_offset = 0
     if newSecond():
         draw_clock()
         draw_digital()
